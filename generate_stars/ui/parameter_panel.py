b3b23a6dc66fdaefa27ae2de2708a57b780250d2
Misha Solodilov <mihsolodilov2015@gmail.com>
2026-04-11 16:47:19 +0300

    Add more convenient function editor
    Signed-off-by: Misha Solodilov <mihsolodilov2015@gmail.com>

--- generate_stars/ui/parameter_panel.py
+++ generate_stars/ui/parameter_panel.py
@@ -56,6 +56,30 @@ class ParameterPanelView(PanelView):
         self.parameter_max_row = self.build_row(localizer.text("ui.label.max"), self.parameter_max_spin)
         self.parameter_fields_box.append(self.parameter_max_row)
 
+        self.parameter_function_editor_button = Gtk.Button(label=localizer.text("ui.button.edit_parameter_function"))
+        self.parameter_function_editor_row = self.build_row(
+            localizer.text("ui.label.parameter_function_editor"),
+            self.parameter_function_editor_button,
+        )
+        self.parameter_fields_box.append(self.parameter_function_editor_row)
+
+        self.parameter_function_editor_window = Gtk.Window(title=localizer.text("ui.window.parameter_function_editor_title"))
+        self.parameter_function_editor_window.set_default_size(720, 460)
+        self.parameter_function_editor_window.set_modal(True)
+        self.parameter_function_editor_window.set_hide_on_close(True)
+        self.parameter_function_editor_window.connect("close-request", self._on_function_editor_close_request)
+
+        self.parameter_function_editor_content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=config.ui.row_spacing)
+        self.parameter_function_editor_content.set_margin_top(config.ui.sidebar_margin)
+        self.parameter_function_editor_content.set_margin_bottom(config.ui.sidebar_margin)
+        self.parameter_function_editor_content.set_margin_start(config.ui.sidebar_margin)
+        self.parameter_function_editor_content.set_margin_end(config.ui.sidebar_margin)
+        self.parameter_function_editor_window.set_child(self.parameter_function_editor_content)
+
+        editor_title = Gtk.Label(label=localizer.text("ui.label.parameter_function_body"), xalign=0.0)
+        editor_title.add_css_class("panel-title")
+        self.parameter_function_editor_content.append(editor_title)
+
         self.parameter_function_body_view = Gtk.TextView()
         self.parameter_function_body_view.set_monospace(True)
         self.parameter_function_body_view.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
@@ -68,19 +92,18 @@ class ParameterPanelView(PanelView):
 
         function_body_scroller = Gtk.ScrolledWindow()
         function_body_scroller.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        function_body_scroller.set_min_content_height(110)
+        function_body_scroller.set_min_content_height(150)
+        function_body_scroller.set_min_content_width(460)
+        function_body_scroller.set_hexpand(True)
+        function_body_scroller.set_vexpand(True)
         function_body_scroller.set_child(self.parameter_function_body_view)
-
-        self.parameter_function_body_row = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=config.ui.row_spacing)
-        function_body_label = Gtk.Label(label=localizer.text("ui.label.parameter_function_body"), xalign=0.0)
-        self.parameter_function_body_row.append(function_body_label)
-        self.parameter_function_body_row.append(function_body_scroller)
-        self.parameter_fields_box.append(self.parameter_function_body_row)
+        self.parameter_function_editor_content.append(function_body_scroller)
 
         self.parameter_preview_label = Gtk.Label(xalign=0.0)
         self.parameter_preview_label.set_wrap(True)
         self.parameter_preview_row = self.build_row(localizer.text("ui.label.parameter_preview"), self.parameter_preview_label)
-        self.parameter_fields_box.append(self.parameter_preview_row)
+        self.parameter_function_editor_content.append(self.parameter_preview_row)
+        self.parameter_function_editor_button.connect("clicked", self._on_open_function_editor)
 
     def apply(self, view_model: ParameterPanelViewModel) -> None:
         self.parameter_enabled_check.set_active(view_model.enabled)
@@ -92,7 +115,7 @@ class ParameterPanelView(PanelView):
         self.parameter_max_spin.set_value(view_model.max_value)
         self.parameter_min_row.set_visible(view_model.show_random_range)
         self.parameter_max_row.set_visible(view_model.show_random_range)
-        self.parameter_function_body_row.set_visible(view_model.show_function_body)
+        self.parameter_function_editor_row.set_visible(view_model.show_function_body)
         self.parameter_preview_row.set_visible(view_model.show_function_preview)
         self.parameter_preview_label.set_text(view_model.function_preview_text)
         self.parameter_preview_label.remove_css_class("status-error")
@@ -100,8 +123,20 @@ class ParameterPanelView(PanelView):
             self.parameter_preview_label.add_css_class("status-error")
         if self._buffer_text(self.parameter_function_body_buffer) != view_model.function_body:
             self.parameter_function_body_buffer.set_text(view_model.function_body)
+        if not view_model.show_function_body:
+            self.parameter_function_editor_window.hide()
 
     def _buffer_text(self, buffer: Gtk.TextBuffer) -> str:
         start = buffer.get_start_iter()
         end = buffer.get_end_iter()
         return buffer.get_text(start, end, True)
+
+    def _on_open_function_editor(self, button: Gtk.Button) -> None:
+        root = self.get_root()
+        if isinstance(root, Gtk.Window):
+            self.parameter_function_editor_window.set_transient_for(root)
+        self.parameter_function_editor_window.present()
+
+    def _on_function_editor_close_request(self, window: Gtk.Window) -> bool:
+        window.hide()
+        return True
